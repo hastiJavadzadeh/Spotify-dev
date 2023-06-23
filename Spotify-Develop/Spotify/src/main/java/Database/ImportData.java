5c5388974a40cc38cfada3cfec86b8bfd34b1b49
Hasti Javadzadeh <hasti83j@gmail.com>
2023-06-24 01:56:30 +0330

    updated

--- Spotify-Develop/Spotify/src/main/java/Database/ImportData.java
+++ Spotify-Develop/Spotify/src/main/java/Database/ImportData.java
@@ -12,11 +12,11 @@ public class ImportData {
     public ImportData() throws SQLException, URISyntaxException {
         ArrayList<Music>musics=new ArrayList<>();
         //TODO create a variable containing absolute path and use it
-        Music SoldierSide=new Music(UUID.randomUUID().toString().hashCode(),"Soldier side","System of a down","Mezmerize","hard rock","3,39","2005",8.9,"C:\\Users\\astan\\Downloads\\Spotify-dev\\Spotify-Develop\\Spotify\\src\\main\\resources\\SoldierSide.mp3");
-        Music BornToTouchYourFeelings=new Music(UUID.randomUUID().toString().hashCode(),"Born To Touch Your Feelings","Scorpions","","hard rock","4,02","2017",9.1,"C:\\Users\\astan\\Downloads\\Spotify-dev\\Spotify-Develop\\Spotify\\src\\main\\resources\\born_to_touch_your_feelings.mp3");
-        Music ILoveYouBillie=new Music(UUID.randomUUID().toString().hashCode(),"I love you","Billie Eilish","","Pop","4,51","2019",9.1,"C:\\Users\\astan\\Downloads\\Spotify-dev\\Spotify-Develop\\Spotify\\src\\main\\resources\\Billie_Eilish_I_Love_You_128.mp3");
-        Music ILoveYouRF=new Music(UUID.randomUUID().toString().hashCode(),"I love you","Rauf & Faik","Запомни I love you","Pop","2,50","2019",9.1,"C:\\Users\\astan\\Downloads\\Spotify-dev\\Spotify-Develop\\Spotify\\src\\main\\resources\\rauf_faik_zapomni_i_love_you 128.mp3");
-        Music That_sNotHowThisWorks=new Music(UUID.randomUUID().toString().hashCode(), "That_s Not How This Works","Charlie Puth","","Pop","2,45","2023",9.1,"C:\\Users\\astan\\Downloads\\Spotify-dev\\Spotify-Develop\\Spotify\\src\\main\\resources\\Charlie Puth - That_s Not How This Works (feat. Dan Shay).mp3");
+        Music SoldierSide=new Music(UUID.randomUUID().toString().hashCode(),"Soldier side","System of a down","Mezmerize","hard rock","3,39","2005",8.9,"C:\\Users\\astan\\Spotify-dev\\Spotify-Develop\\Spotify\\src\\main\\resources\\SoldierSide.mp3");
+        Music BornToTouchYourFeelings=new Music(UUID.randomUUID().toString().hashCode(),"Born To Touch Your Feelings","Scorpions","","hard rock","4,02","2017",9.1,"C:\\Users\\astan\\Spotify-dev\\Spotify-Develop\\Spotify\\src\\main\\resources\\born_to_touch_your_feelings.mp3");
+        Music ILoveYouBillie=new Music(UUID.randomUUID().toString().hashCode(),"I love you","Billie Eilish","","Pop","4,51","2019",9.1,"C:\\Users\\astan\\Spotify-dev\\Spotify-Develop\\Spotify\\src\\main\\resources\\Billie_Eilish_I_Love_You_128.mp3");
+        Music ILoveYouRF=new Music(UUID.randomUUID().toString().hashCode(),"I love you","Rauf & Faik","Запомни I love you","Pop","2,50","2019",9.1,"C:\\Users\\astan\\Spotify-dev\\Spotify-Develop\\Spotify\\src\\main\\resources\\rauf_faik_zapomni_i_love_you 128.mp3");
+        Music That_sNotHowThisWorks=new Music(UUID.randomUUID().toString().hashCode(), "That_s Not How This Works","Charlie Puth","","Pop","2,45","2023",9.1,"C:\\Users\\astan\\Spotify-dev\\Spotify-Develop\\Spotify\\src\\main\\resources\\Charlie Puth - That_s Not How This Works (feat. Dan Shay).mp3");
 
         musics.add(SoldierSide);
         musics.add(BornToTouchYourFeelings);
